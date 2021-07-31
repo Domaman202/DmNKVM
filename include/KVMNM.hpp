a77aff24f9784130e88f71146003a2ce3e145931
Domaman202 <vip.domaman@mail.ru>
2021-07-31 22:44:32 +0700

    Update KVMNM.hpp

--- include/KVMNM.hpp
+++ include/KVMNM.hpp
@@ -10,21 +10,36 @@ namespace DmN::KVM {
     /// Пространство имён
     exStruct(Namespace) : Nameble {
         /// Предок
-        Namespace* parent;
+        Namespace *parent;
+    };
+
+    exStruct(ESModule) : Namespace {
+        /// Enum-ы
+        SDL::Node<struct EnumBase *> *enums;
+    };
+
+    exStruct(SSModule) : Namespace {
+        /// Структуры
+        SDL::Node<struct StructBase *> *structures;
+    };
+
+    exStruct(CSModule) : Namespace {
+        /// Классы
+        SDL::Node<struct ClassBase *> *classes;
+    };
+
+    exStruct(FSModule) : Namespace {
+        /// Переменные
+        SDL::Node<struct LocalVariable_t *> *variables;
+    };
+
+    exStruct(MSModule) : Namespace {
+        /// Функции
+        SDL::Node<struct Method_t *> *methods;
     };
 
     /// Модуль
-    exStruct(Module) : Namespace {
-        /// Enum-ы
-        SDL::Node<struct EnumBase*>* enums;
-        /// Структуры
-        SDL::Node<struct StructBase*>* structures;
-        /// Классы
-        SDL::Node<struct ClassBase*>* classes;
-        /// Функции
-        SDL::Node<struct Method_t*>* methods;
-        /// Переменные
-        SDL::Node<struct LocalVariable_t*>* variables;
+    exStruct(Module) : ESModule, SSModule, CSModule, FSModule, MSModule {
     };
 
     /// (Namespace Storage) Хранилище пространств имён
@@ -34,42 +49,42 @@ namespace DmN::KVM {
          * @param ns - пространство имён для добавления
          * @return ID нового пространства имён
          */
-        virtual NSI_t addNew(Namespace* ns) = 0;
+        virtual NSI_t addNew(Namespace *ns) = 0;
 
         /*!
          * Добавляет новое пространство имён, если оно уже существует то возвращает его ID
          * @param ns - пространство имён для добавления
          * @return ID пространства имён
          */
-        virtual NSI_t add(Namespace* ns) = 0;
+        virtual NSI_t add(Namespace *ns) = 0;
 
         /*!
          * Получает ID пространства имён
          * @param ns - пространство имён ID которого нужно получить
          * @return ID нужного нам пространства имён
          */
-        virtual NSI_t get(Namespace* ns) = 0;
+        virtual NSI_t get(Namespace *ns) = 0;
 
         /*!
          * Получает пространство имён по ID
          * @param id - ID для получаения пространства имён
          * @return Нужное пространство имён
          */
-        virtual Namespace* get(NSI_t id) = 0;
+        virtual Namespace *get(NSI_t id) = 0;
 
         /*!
          * Удаляет пространство имён
          * @param ns - пространство имён для удаления
          * @return ID пространства имён
          */
-        virtual NSI_t remove(Namespace* ns) = 0;
+        virtual NSI_t remove(Namespace *ns) = 0;
 
         /*!
          * Удаляет пространство имён по ID
          * @param id - ID пространства имён для удаления
          * @return удалённое пространство имён
          */
-        virtual Namespace* remove(NSI_t id) = 0;
+        virtual Namespace *remove(NSI_t id) = 0;
 
         /*!
          * Очищает хранилище
@@ -82,6 +97,7 @@ namespace DmN::KVM {
         explicit NSObject(NSI_t ns) {
             this->ns = ns;
         }
+
         /// (Namespace ID) ID пространства имён
         NSI_t ns;
     };
