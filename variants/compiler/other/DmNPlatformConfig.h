58bd316aabd979b224223333d4b36d0ea4effa0b
Domaman202 <vip.domaman@mail.ru>
2021-08-17 17:47:27 +0700

    .

--- variants/compiler/other/DmNPlatformConfig.h
+++ variants/compiler/other/DmNPlatformConfig.h
@@ -2,8 +2,8 @@
 #ifndef DMNKVM_DMNCONFIG_H
 #define DMNKVM_DMNCONFIG_H
 
-#define DMN_KVM_E
-#define DMN_KVM_ET(type, name) type name
+#define DMN_KVM_E extern
+#define DMN_KVM_ET(type, name) DMN_KVM_E type name
 #define DMN_KVM_EE(name) enum name
 #define DMN_KVM_ES(name) struct name
 #define DMN_KVM_EC(name) class name
