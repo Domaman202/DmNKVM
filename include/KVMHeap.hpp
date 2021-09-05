a7d6fe2b1d1670cc6f8c93deedd0638394a9eafd
Domaman202 <vip.domaman@mail.ru>
2021-09-05 13:01:56 +0700

    Update KVMHeap.hpp

--- include/KVMHeap.hpp
+++ include/KVMHeap.hpp
@@ -15,14 +15,14 @@ namespace DmN::KVM {
          * @param clazz Класс для добавления
          * @return ID добавленного класса в хипе
          */
-        virtual CI_t addNew(ClassBase* clazz) = 0;
+        virtual CI_t addNew(ClassBase *clazz) = 0;
 
         /*!
          * Добавляет новый класс в хип, если класс уже был добавлен то просто возвращает его ID
          * @param clazz Класс для добавления
          * @return ID добавленного класса
          */
-        virtual CI_t add(ClassBase* clazz) = 0;
+        virtual CI_t add(ClassBase *clazz) = 0;
 
         /*!
          * Заменяет уже существующий класс по ID на новый
@@ -30,26 +30,26 @@ namespace DmN::KVM {
          * @param id ID класса который нужно заменить
          * @return Заменённый (оригинальный) класс
          */
-        virtual ClassBase* replace(ClassBase* clazz, CI_t id) = 0;
+        virtual ClassBase *replace(ClassBase *clazz, CI_t id) = 0;
 
         /*!
          * Удаляет класс из хипа
          * @param clazz Класс для удаления
          */
-        virtual void remove(ClassBase* clazz) = 0;
+        virtual void remove(ClassBase *clazz) = 0;
 
         /*!
          * Удаляет класс из хипа
          * @param id ID класса для удаления
          * @return Класс который был удалён
          */
-        virtual ClassBase* remove(CI_t id) = 0;
+        virtual ClassBase *remove(CI_t id) = 0;
 
         /*!
          * Удаляет класс из хипа и высвобождает память из под него
          * @param clazz Класс для удаления
          */
-        virtual void collect(ClassBase* clazz) = 0;
+        virtual void collect(ClassBase *clazz) = 0;
 
         /*!
          * Удаляет класс из хипа и высвобождает память из под него
@@ -62,28 +62,34 @@ namespace DmN::KVM {
          * @param clazz Класс ID которого нужно получить
          * @return ID класса (0 в случае неудачи)
          */
-        virtual CI_t get(ClassBase* clazz) = 0;
+        virtual CI_t get(const ClassBase *clazz) = 0;
 
         /*!
          * Получает класс из хипа по его ID
          * @param id ID класса для получения
          * @return Нужный нам класс (nullptr в случае неудачи)
          */
-        virtual ClassBase* get(CI_t id) = 0;
+        virtual ClassBase *get(CI_t id) = 0;
 
         /*!
          * Получает предков класса
          * @param clazz Класс предков которого нужно получить
          * @return Пара [массив предков, кол-во предков] данного класса
          */
-        virtual SDL::pair<ClassBase**, size_t> getParents(ClassBase* clazz) = 0;
+        virtual SDL::pair<ClassBase **, size_t> getParents(const ClassBase *clazz) = 0;
 
         /*!
          * Получает предков класса
          * @param clazz ID класса в хипе предков которого нужно получить
          * @return Пара [массив предков, кол-во предков] данного класса
          */
-        virtual SDL::pair<ClassBase**, size_t> getParents(CI_t clazz) = 0;
+        virtual SDL::pair<ClassBase **, size_t> getParents(CI_t clazz) = 0;
+
+        /*!
+         * Добавляет объекты в текущий хип из другого
+         * @param heap хип из которого мы будет добавлять объекты
+         */
+        virtual void operator+(const Heap *heap) = 0;
     };
 }
 
