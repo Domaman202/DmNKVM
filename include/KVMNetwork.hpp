a9468e4914af997b8d271ae7a3610d7a25ba7ead
Domaman202 <vip.domaman@mail.ru>
2021-09-07 16:04:41 +0700

    .

--- include/KVMNetwork.hpp
+++ include/KVMNetwork.hpp
@@ -13,8 +13,83 @@ using namespace DmN::KVM::Error;
 
 namespace DmN::KVM::Network {
     DMN_KVM_E struct NetworkObject {
+    public:
         sockaddr_in addr{};
         int _socket = 0;
+    };
+
+    DMN_KVM_E class SocketConnection : public NetworkObject {
+    public:
+        /*!
+         * Отправляет буфер
+         * @param buf буфер для отправки
+         * @param len размер буфера
+         * @return TODO: ЯХЗ
+         */
+        inline ssize_t sendBuf(void* buf, size_t len) const {
+            return send(_socket, buf, len, 0);
+        }
+
+        /*!
+         * Отправляет msghdr
+         * @param msg msghdr для отправки
+         * @return TODO: ЯХЗ
+         */
+        inline ssize_t sendMsg(msghdr* msg) const {
+            return sendmsg(_socket, msg, 0);
+        }
+
+        /*!
+         * Читает входящие в буфер
+         * @param buf буфер в который нужно записать входящие данные
+         * @param len размер данных которые нужно записать
+         * @return TODO: ЯХЗ
+         */
+        inline ssize_t readBuf(void* buf, size_t len) const {
+            return recv(_socket, buf, len, 0);
+        }
+
+        /*!
+         * Читает входящий msghdr
+         * @param msg msghdr в который нужно читать входящие данные
+         * @return TODO: ЯХЗ
+         */
+        inline ssize_t readMsg(msghdr* msg) const {
+            return recvmsg(_socket, msg, 0);
+        }
+    };
+
+    DMN_KVM_E class Client : public SocketConnection {
+    public:
+        Client(const std::string &address, bool ipv6, uint16_t port, NWR &result, int &error) {
+            // Проверка на ipv6
+            int ip_protocol = ipv6 ? AF_INET6 : AF_INET;
+
+            // Создаём сокет
+            if ((_socket = socket(ip_protocol, SOCK_STREAM, PF_UNSPEC)) < 0) [[unlikely]] {
+                result = Error::SOCKET_CREATE_ERROR;
+                error = _socket;
+                return;
+            }
+
+            // Выставляет сетевые параметры
+            addr.sin_family = ip_protocol;
+            addr.sin_port = port;
+            if ((error = inet_pton(ip_protocol, address.c_str(), &(addr.sin_addr))) != 1) [[unlikely]] {
+                result = Error::IP_CONVERT_ERROR;
+                return;
+            }
+
+            // Если мы ещё не померли, то возвращаем хороший результат
+            result = (NWR) Error::SUCCESS;
+            error = 0;
+        }
+
+        inline NWR tryConnect(int &error) {
+            if (connect(_socket, (sockaddr*) &addr, sizeof(addr)) < 0) [[unlikely]]
+                return Error::CONNECT_ERROR;
+            return (NWR) Error::SUCCESS;
+        }
     };
 
     DMN_KVM_E class Server : public NetworkObject {
@@ -75,87 +150,19 @@ namespace DmN::KVM::Network {
             return (NWR) Error::SUCCESS;
         }
 
-        NWR accept_(int& error) { // TODO: rework with SocketConnexion
+        SocketConnection* acceptConnection(NWR& result, int& error) {
             auto addr_size = sizeof(addr);
-            if ((error = accept(_socket, (sockaddr*) &addr, (socklen_t*) &addr_size)) < 0) [[unlikely]]
-                return (NWR) Error::ACCEPT_ERROR;
-            return (NWR) Error::SUCCESS;
-        }
-    };
-
-    DMN_KVM_E class Client : public NetworkObject {
-    public:
-        Client(const std::string &address, bool ipv6, uint16_t port, NWR &result, int &error) {
-            // Проверка на ipv6
-            int ip_protocol = ipv6 ? AF_INET6 : AF_INET;
-
-            // Создаём сокет
-            if ((_socket = socket(ip_protocol, SOCK_STREAM, PF_UNSPEC)) < 0) [[unlikely]] {
-                result = Error::SOCKET_CREATE_ERROR;
-                error = _socket;
-                return;
+            if ((error = accept(_socket, (sockaddr*) &addr, (socklen_t*) &addr_size)) < 0) [[unlikely]] {
+                result = (NWR) Error::ACCEPT_ERROR;
+                return nullptr;
             }
 
-            // Выставляет сетевые параметры
-            addr.sin_family = ip_protocol;
-            addr.sin_port = port;
-            if ((error = inet_pton(ip_protocol, address.c_str(), &(addr.sin_addr))) != 1) [[unlikely]] {
-                result = Error::IP_CONVERT_ERROR;
-                return;
-            }
-
-            // Если мы ещё не померли, то возвращаем хороший результат
             result = (NWR) Error::SUCCESS;
-            error = 0;
-        }
 
-        inline NWR tryConnect(int &error) {
-            if (connect(_socket, (sockaddr*) &addr, sizeof(addr)) < 0) [[unlikely]]
-                return Error::CONNECT_ERROR;
-            return (NWR) Error::SUCCESS;
+            auto connection = new SocketConnection();
+            connection->_socket = error;
+            return connection;
         }
-
-        /*!
-         * Отправляет буфер
-         * @param buf буфер для отправки
-         * @param len размер буфера
-         * @return TODO: ЯХЗ
-         */
-        inline ssize_t sendBuf(void* buf, size_t len) const {
-            return send(_socket, buf, len, 0);
-        }
-
-        /*!
-         * Отправляет msghdr
-         * @param msg msghdr для отправки
-         * @return TODO: ЯХЗ
-         */
-        inline ssize_t sendMsg(msghdr* msg) const {
-            return sendmsg(_socket, msg, 0);
-        }
-
-        /*!
-         * Читает входящие в буфер
-         * @param buf буфер в который нужно записать входящие данные
-         * @param len размер данных которые нужно записать
-         * @return TODO: ЯХЗ
-         */
-        inline ssize_t readBuf(void* buf, size_t len) const {
-            return recv(_socket, buf, len, 0);
-        }
-
-        /*!
-         * Читает входящий msghdr
-         * @param msg msghdr в который нужно читать входящие данные
-         * @return TODO: ЯХЗ
-         */
-        inline ssize_t readMsg(msghdr* msg) const {
-            return recvmsg(_socket, msg, 0);
-        }
-    };
-
-    class SocketConnexion : public NetworkObject {
-        // TODO:
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <KVMNetwork.hpp>
 #include <thread>
+#include <iostream>
 
 using namespace DmN::KVM::Network;
 
@@ -8,6 +9,10 @@ void networkTestS();
 void networkTestC();
 
 int main(int argc, char* argv[]) {
+    std::thread server_thread(networkTestS);
+    std::thread client_thread(networkTestC);
+    server_thread.join();
+    client_thread.join();
     return 0;
 }
 
@@ -15,8 +20,33 @@ void networkTestS() {
     NWR nwr;
     int error;
     //
-    auto server = new Server(0, nwr, error);
+    auto server = new Server(228, nwr, error);
+    check(nwr);
     //
-    server->listen_(3, error);
+    check(server->listen_(3, error));
     //
+    auto connection = server->acceptConnection(nwr, error);
+    check(nwr);
+    //
+    connection->sendBuf((void *) "Hello, World!", 13);
+    //
+}
+
+void networkTestC() {
+    NWR nwr;
+    int error;
+    //
+    auto client = new Client("0.0.0.0", false, 228, nwr, error);
+    check(nwr);
+    //
+    check(client->tryConnect(error));
+    //
+    char* str = new char[13];
+    client->readBuf(str, 13);
+    std::cout << str << std::endl;
+}
+
+inline void check(NWR nwr) {
+    if (nwr != DmN::KVM::Error::SUCCESS)
+        throw;
 }
