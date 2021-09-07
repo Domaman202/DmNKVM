76546926627a5f46f317e29dd84da81f755a8c3e
Domaman202 <vip.domaman@mail.ru>
2021-09-07 21:37:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,31 +14,37 @@ void networkTestS();
 void networkTestC();
 
 int main(int argc, char* argv[]) {
+    // Указываем порт для тестов
     __PORT__ = 228;
+    // Создаём поток сервера
     std::thread server_thread(networkTestS);
+    // Создаём поток клиента
     std::thread client_thread(networkTestC);
+    // Подключаемся к потоку клиента
     client_thread.join();
+    // Подключаемся к потоку сервера
     server_thread.join();
-
+    // Если мы ещё не сдохли, то всё норм, возвращаем 0
     return 0;
 }
 
 void networkTestS() {
+    // Переменные ошибки
     NWR nwr;
     int error;
-    //
+    // Создаём сервер
     auto server = new Server(__PORT__, nwr, error);
     check(nwr);
-    //
+    // Запускаем просмотр клиентов
     check(server->listen_(1, error));
-    //
+    // Подключаемся к клиенту
     auto connection = server->accept(nwr, error);
     check(nwr);
-    //
+    // Отправляем сообщение (с выравниванием до 1024 байт)
     auto chars = new char[1024];
-    memcpy(chars, "Hello, World!", 13);
+    strcpy(chars, "Hello, World!");
     connection->sendBuf(chars, 1024);
-    //
+    // Закрываем соединения
     connection->close();
     server->close();
 }
@@ -46,17 +52,17 @@ void networkTestS() {
 void networkTestC() {
     NWR nwr;
     int error;
-    //
+    // Создаём клиент
     auto client = new Client("0.0.0.0", false, __PORT__, nwr, error);
     check(nwr);
-    //
+    // Коннектимся к серверу
     check(client->tryConnect(error));
-    //
+    // Принимаем сообщение выровненное до 1024 байт и выводим его
     char* str = new char[1024];
     client->readBuf(str, 1024);
     std::cout << str << std::endl;
     delete[] str;
-    //
+    // Закрываем соединение
     client->close();
 }
 
