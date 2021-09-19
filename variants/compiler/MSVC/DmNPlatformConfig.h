0f6487c17c7d5eea179ecc78cca1ad3ef3752c54
Domaman202 <vip.domaman@mail.ru>
2021-09-19 15:07:21 +0700

    .

--- variants/compiler/MSVC/DmNPlatformConfig.h
+++ variants/compiler/MSVC/DmNPlatformConfig.h
@@ -2,14 +2,4 @@
 #ifndef DMN_KVM_PLATFORM_CONFIG_H
 #define DMN_KVM_PLATFORM_CONFIG_H
 
-//#ifndef DMN_KVM_EXPORT
-//#define DMN_KVM_E __declspec(dllexport)
-//#elif
-//#define DMN_KVM_E __declspec(dllimport)
-//#endif
-
-//#define DMN_KVM_EE extern
-//#define DMN_KVM_EE
-//#define DMN_KVM_EF DMN_KVM_EE DMN_KVM_EE
-
 #endif /* DMN_KVM_PLATFORM_CONFIG_H */
--- variants/compiler/other/DmNPlatformConfig.h
+++ variants/compiler/other/DmNPlatformConfig.h
@@ -2,8 +2,4 @@
 #ifndef DMNKVM_DMNCONFIG_H
 #define DMNKVM_DMNCONFIG_H
 
-#define DMN_KVM_E
-#define DMN_KVM_EE extern
-#define DMN_KVM_EF DMN_KVM_EE
-
 #endif /* DMNKVM_DMNCONFIG_H */
