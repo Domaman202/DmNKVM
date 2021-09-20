9637246b310b6148a23f76a9c00392134c697fd2
Domaman202 <vip.domaman@mail.ru>
2021-09-20 18:18:46 +0700

    Update KVMVM.hpp

--- include/KVMVM.hpp
+++ include/KVMVM.hpp
@@ -4,8 +4,21 @@
 #ifndef DMN_KVM_VM_HPP
 #define DMN_KVM_VM_HPP
 
-namespace DmN::KVM::VM {
+#include "KVMCall.hpp"
+#include "KVMThread.hpp"
 
+namespace DmN::KVM::VM {
+    /// Контекст выполнения
+    struct ExecuteContext {
+        /// Текущий вызов
+        Call::Call* lastCall;
+        /// Текущий поток
+        Thread* lastThread;
+        /// Текущий указатель кода
+        uint64_t lastBcPtr;
+        /// Предыдущий вызов
+        ExecuteContext* prevContext;
+    };
 }
 
 #endif /* DMN_KVM_VM_HPP */
