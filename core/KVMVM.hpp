b9aceb6b68f723f4569e91d6d3eebf9308f81abd
Domaman202 <vip.domaman@mail.ru>
2021-09-25 19:22:02 +0700

    .

--- core/KVMVM.hpp
+++ core/KVMVM.hpp
@@ -11,6 +11,10 @@
 #include "KVMSR.hpp"
 
 namespace DmN::KVM::VM {
+    enum ByteCode {
+
+    };
+
     /// Контекст выполнения
     struct ExecuteContext {
         /// Текущий вызов
@@ -58,8 +62,14 @@ namespace DmN::KVM::VM {
             createMain(main, mainSS, code, cs);
         }
 
-        void* callMain() {
+        Value_t* callMain() {
             // TODO:
+        }
+
+        Value_t* evalBC(ExecuteContext c, uint8_t* b, size_t bc) {
+            for (size_t* i = &c.lastBcPtr; *i < bc; (*i)++) {
+
+            }
         }
 
         static void createMain(BCMethod_t *ptr, SS* ss, uint8_t *code, size_t cs) {
@@ -69,86 +79,3 @@ namespace DmN::KVM::VM {
 }
 
 #endif /* DMN_KVM_VM_HPP */
-
-/*
-    Разметка кода:
-    0x0 - 0x10 Кол-во регистров главного потока
-    0x10 - 0xXXX Код
- */
-/*
-    Байт код:
-    |0x0| Ox1 (RX), Ox2 (RY)
-    MR (RX, RY)
-    Move Register
-    [RX to RY]
-
-    |0x1 - 0x4| Ox1 (RX), Ox2 (RY)
-    MRT (RX, RY)
-    Move Register with Type
-    [(LL,HH,LH,HL) bytes, RX to RY]
-
-    |0x5| 0x1 (RX)
-    PhS (RX)
-    Push to Stack
-    [RX to stack]
-
-    |0x6| 0x1 (RX)
-    Pp (RX)
-    Pop to RX
-    [Stack to RX]
-
-    |0x7| 0x1 (RX)
-    Pk (RX)
-    Peek to RX
-    [Stack to RX]
-
-    |0x8| 0x1 (RX), 0x2 (RY)
-    DR (RX, RY)
-    Dereference Register
-    [REGY = *REGX]
-
-    |0x9| 0x1 (RX), 0x2 (RY)
-    RR (RX, RY)
-    Reference Register
-    [REGY = (void*) REGX]
-
-    |0xA| 0x1 (RX), 0x2 (TYPE)
-    CTV (RX, TYPE)
-    Convert To Val
-    [RX = new Value_t(RX, TYPE, false)]
-
-    |0xB| 0x1 (RX), 0x2 (TYPE)
-    CTCV (RX, TYPE)
-    Convert To Collecteble Val
-    [RX = new Value_t(RX, TYPE, false)]
-
-    |0xC| 0x1 (RX)
-    CFV (RX)
-    UnConvert Of Value
-    [RX = RX->value]
-
-    |0xD| 0x1 (RX)
-    CFVADV (RX)
-    UnConvert Of Value And Delete Value
-    [RX = RX->value]
-
-    |0xF - 0x15| 0x1 (RX), 0x2 (RY), 0x3 (RZ)
-    MR (RX, RY, RZ)
-    Math Register
-    [RZ = RX TYPE (TYPE: ADD; SUB; MUL; DIV; POW; SQRT) RY]
-
-    |0x16| 0x1 (RX), 0x2 (BYTE)
-    LTR1 (RX, BYTE)
-    Load To Register
-    [RX = BYTE]
-
-    |0x17| 0x1 (RX), 0x2 (BYTE0), 0x3 (BYTE1)
-    LTR2 (RX, BYTE0, BYTE1)
-    Load To Register
-    [RX = BYTE0 | (BYTE1 << 8)]
-
-    |0x18| 0x1 (RX), 0x2 (BYTE0), 0x3 (BYTE1), 0x4 (BYTE2), 0x5(BYTE3))
-    LTR4 (RX, BYTE0, BYTE1, BYTE2, BYTE3)
-    Load To Register
-    [RX = BYTE0 | (BYTE1 << 8) | BYTE2 << 16) | (BYTE3 << 24)]
- */
